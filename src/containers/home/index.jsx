b9ca7a02d9c03a13feba79952e8066204e5c4e1e
V <vedantdalal100@gmail.com>
2024-02-09 10:31:04 +0530

    modified:   src/containers/home/index.jsx 	modified:   src/containers/home/styles.scss

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,18 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Animate } from "react-simple-animate";
 import "./styles.scss";
 
-const Home = ({ toggleIcon }) => {
+const Home = () => {
   const navigate = useNavigate();
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-    return () => {
-      // Cleanup logic if needed
-    };
-  }, []);
 
   const handleNavigateToContactMePage = () => {
     navigate("/contact");
@@ -20,38 +12,29 @@ const Home = ({ toggleIcon }) => {
 
   return (
     <section id="home" className="home">
-      {!toggleIcon && (
-        <>
-          <div className="home__text-wrapper">
-            <h1>
-              Hello, I'm Vedant Dalal,
-              <br />
-              A web developer
-            </h1>
-          </div>
-          {isMounted && (
-            <Animate
-              play
-              duration={1.5}
-              delay={1}
-              fps={60}
-              start={{
-                transform: "translateY(550px)",
-              }}
-              end={{
-                transform: "translateY(0)",
-              }}
-              easeType="cubic-bezier(0.68, -0.55, 0.27, 1.55)"
-            >
-              <div className="home__contact-me">
-                <button onClick={handleNavigateToContactMePage}>Hire Me</button>
-              </div>
-            </Animate>
-          )}
-        </>
-      )}
+      <div className="home__text-wrapper">
+        <h1>
+          Hello, I'm Vedant Dalal
+          <br />
+          A Web Developer
+        </h1>
+      </div>
+      <Animate
+        play
+        duration={1.5}
+        delay={1}
+        start={{
+          transform: "translateY(550px)",
+        }}
+        end={{
+          transform: "translatex(0px)",
+        }}
+      >
+        <div className="home__contact-me">
+          <button onClick={handleNavigateToContactMePage}>Hire Me</button>
+        </div>
+      </Animate>
     </section>
   );
 };
-
 export default Home;
